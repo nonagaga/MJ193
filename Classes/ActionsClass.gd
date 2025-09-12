bc29b7af1ac99adf1bcb93bf3e1bca45fcb19775
KingCorvid <91034705+KingCorvid@users.noreply.github.com>
2025-09-12 12:58:03 -0400

    added some comments

--- Classes/ActionsClass.gd
+++ Classes/ActionsClass.gd
@@ -1,5 +1,6 @@
 extends Resource
 class_name Action
 
+#function that is called when an action resolves
 func resolve(targets:Array[Enemy]):
 	print(get_class())
--- Classes/CardClass.gd
+++ Classes/CardClass.gd
@@ -1,7 +1,9 @@
 extends Resource
 class_name Card
+#how expensive is this to buy from the shop
 @export var price:int = 0
 @export var damage:int = 0
+#how many enemies must this card target
 @export var maxTargets:int = 0
 #number of other cards that must be discarded to play this card
 @export var discardCost:int = 0
@@ -12,14 +14,17 @@ class_name Card
 #array of enemies the card is targetting
 var targets:Array[Enemy]
 
+#when drawn execute all draw effects
 func drawEffect():
 	for i:Action in drawActions:
 		i.resolve(targets)
 
+#when played execute all play effects
 func playEffect():
 	for i:Action in playActions:
 		i.resolve(targets)
 
+#when discarded execute all discard effects
 func discardEffect():
 	for i:Action in discardActions:
 		i.resolve(targets)
