55f6f64707903fa4fbaf401319a1f3047d49fa5a
KingCorvid <91034705+KingCorvid@users.noreply.github.com>
2025-09-13 15:00:59 -0400

    New Card

--- Actions/Action Classes/ApplyWeakenActionClass.gd
+++ Actions/Action Classes/ApplyWeakenActionClass.gd
@@ -1,1 +1,10 @@
 extends Action
+class_name WeakenAction
+@export var weakenStacks:int
+
+func resolve(targets:Array[Enemy]):
+	for i in targets:
+		i.weakened+=weakenStacks
+
+func updateText():
+	actualText = text % str(weakenStacks)
