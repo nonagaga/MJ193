26c5f7222ab687f1fd4e4358f2156778110d2f42
KingCorvid <91034705+KingCorvid@users.noreply.github.com>
2025-09-13 20:32:02 -0400

    enter target mode

--- UI/game_ui.gd
+++ UI/game_ui.gd
@@ -8,11 +8,12 @@ var card_packed = preload("res://card.tscn")
 @onready var end_turn: Button = $"Player Choices/End Turn"
 
 func _ready()->void:
+	await get_tree().process_frame
 	play.disabled = true
 	cancel.disabled = true
-	
 	if Globals.gameManager:
 		Globals.gameManager.card_drawn.connect(on_card_drawn)
+	print(Globals.hand)
 
 func on_card_drawn(card:CardDataClass):
 	if card == null:
@@ -23,15 +24,16 @@ func create_card(card:CardDataClass)->Control:
 	var new_card_instance = card_packed.instantiate()
 	new_card_instance.card_res = card.duplicate()
 	$Void.add_child(new_card_instance)
+	new_card_instance.selected.connect(card_selected.bind(new_card_instance))
 	return new_card_instance
 
 func enter_target_mode():
 	for i in get_tree().get_nodes_in_group("Enemy"):
-		i.enabled = true
+		i.disabled = false
 		i.target_highlight.visible = true
 
 func card_selected(item:Control):
-	print("yipee")
+	cur_card_selected = item.card_res
 	if cur_card_selected!=null:
 		if cur_card_selected.doesCardTarget():
 			enter_target_mode()
